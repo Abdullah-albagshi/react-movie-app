0b5ebb8afe215ab9f3c3bdea40c4be270943c94b
King1419 <abdullah.albagshi@gmail.com>
2020-12-24 22:59:32 +0300

    refactor:add condition to popover overview string length

--- src/Components/Common/MovieDetails.jsx
+++ src/Components/Common/MovieDetails.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Popover, OverlayTrigger } from 'react-bootstrap';
 
-const MovieDetails = ({ title, overview, release_date, vote_average }) => {
+const MovieDetails = ({ title, overview, release_date }) => {
 	const popover = (
 		<Popover id="popover-basic">
 			<Popover.Title as="h3">{title}</Popover.Title>
-			<Popover.Content>{overview}</Popover.Content>
+			<Popover.Content>{overview.length>=300 ? `${overview.slice(0,300)}...` :overview.length===0 ? "No over view for this movie" : overview}</Popover.Content>
 		</Popover>
 	);
 
