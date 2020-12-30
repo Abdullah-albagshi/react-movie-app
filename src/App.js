c224b2519bcfb9bbbcac3a6bb9d0f7fb8f428e8d
King1419 <abdullah.albagshi@gmail.com>
2020-12-30 20:14:52 +0300

    feat:add routing, navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
 import Movie from './Components/Movie';
+import SelectedMovie from './Components/SelectedMovie';
+import NavbarComponent from './Components/NavbarComponent';
 
 function App() {
-	return <div className="container-fluid  mt-4">{<Movie />}</div>;
+	return (
+		<>
+			<NavbarComponent />
+			<div className="container-fluid  mt-4">
+				<Switch>
+					<Route exact path="/" component={Movie} />
+					<Route path="/movie/:id" component={SelectedMovie} />
+				</Switch>
+			</div>
+		</>
+	);
 }
 
 export default App;
