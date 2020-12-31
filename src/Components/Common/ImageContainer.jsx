832162ce69b811d13895e0817305eea3671a8cfc
King1419 <abdullah.albagshi@gmail.com>
2020-12-31 12:59:49 +0300

    refactor: for reusability in another component

--- src/Components/Common/ImageContainer.jsx
+++ src/Components/Common/ImageContainer.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React from 'react';
 const imgUrl = 'https://image.tmdb.org/t/p/w500/';
 
-const ImageContainer = ({poster_path,title}) => {
-    return (
-        <div className="img-card">
-        <img className='poster-img' src={`${imgUrl}${poster_path}`} alt={`${title}`} />
-    </div>
-)
-}
+const ImageContainer = ({ poster_path, title, defClass = 'poster-img' }) => {
+	return (
+		<div className="img-card">
+			<img
+				loading="lazy"
+				className={defClass}
+				src={`${imgUrl}${poster_path}`}
+				alt={`${title}`}
+			/>
+		</div>
+	);
+};
 
-export default ImageContainer
+export default ImageContainer;
