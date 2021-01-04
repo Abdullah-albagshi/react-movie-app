0d55f6337d0c9636ff3e10d5c297b1dbc288bda7
King1419 <abdullah.albagshi@gmail.com>
2021-01-05 00:42:05 +0300

    feat: add routing for search results

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Movie from './Components/Movie';
 import SelectedMovie from './Components/SelectedMovie';
 import NavbarComponent from './Components/NavbarComponent';
 import TopMovies from './Components/TopMovies';
+import Results from './Components/Results';
 
 function App() {
 	return (
@@ -12,8 +13,20 @@ function App() {
 			<div className="container-fluid  mt-4">
 				<Switch>
 					<Route exact path="/" component={Movie} />
-					<Route exact path="/movie/:id" component={SelectedMovie} />
+					<Route
+						exact
+						path="/movie/:id"
+						component={SelectedMovie}
+					/>
 					<Route exact path="/top-rated" component={TopMovies} />
+				</Switch>
+				<Switch>
+					<Route exact path="/results" component={Results}/>
+					<Route
+						exact
+						path="/results/:id?/:mediaType?"
+						component={SelectedMovie}
+					/>
 				</Switch>
 			</div>
 		</>
