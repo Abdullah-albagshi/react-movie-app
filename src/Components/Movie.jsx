9286f1f72d0466cfd3f398122eb1c7693f1f4653
King1419 <abdullah.albagshi@gmail.com>
2021-01-07 15:18:08 +0300

    feat:add filter compnent to Movie parent component

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import MovieCard from './MovieCard';
 import Paginate from './Common/Paginate';
+import FilterBar from './Common/FilterBar';
 
 const apiKey = process.env.REACT_APP_API_KEY;
 const words = [
@@ -16,6 +17,7 @@ export default class movie extends Component {
 			moviesObj: [],
 			pageNum: 1,
 			linkPath: 'movie',
+			genre: '',
 		};
 	}
 	path = 'movie';
@@ -33,9 +35,11 @@ export default class movie extends Component {
 		}
 	}
 
-	async getMovies() {
+	async getMovies(genres = '') {
 		let url = this.pathName();
-		const movies = await axios.get(url + this.state.pageNum);
+		const movies = await axios.get(
+			url + this.state.pageNum + this.state.genre
+		);
 		const moviesData = movies.data.results;
 		this.setState({ moviesObj: moviesData });
 	}
@@ -44,7 +48,11 @@ export default class movie extends Component {
 		const pageNumber = JSON.parse(
 			localStorage.getItem('savedPageNumber') || 1
 		);
-		this.setState({ pageNum: pageNumber }, () => {
+		const selectedGenre = JSON.parse(
+			localStorage.getItem('savedSelectedGenre') || 1
+		);
+
+		this.setState({ pageNum: pageNumber, genre: selectedGenre }, () => {
 			try {
 				this.getMovies();
 			} catch (error) {
@@ -57,6 +65,10 @@ export default class movie extends Component {
 		localStorage.setItem(
 			'savedPageNumber',
 			JSON.stringify(this.state.pageNum)
+		);
+		localStorage.setItem(
+			'savedSelectedGenre',
+			JSON.stringify(this.state.genre)
 		);
 	}
 
@@ -85,51 +97,66 @@ export default class movie extends Component {
 	isDisabled = () => {
 		if (this.state.pageNum === 1) return true;
 	};
+
+	handelFilter = (genres) => {
+		let urlGenres = `&with_genres=${genres.join('%2C')}`;
+		this.setState({ genre: urlGenres }, () => {
+			this.getMovies(this.state.genre);
+		});
+	};
 	render() {
 		return (
 			<>
-				<div className="row d-flex justify-content-around">
-					{this.state.moviesObj.map((movie) => {
-						if (
-							movie.overview.includes('sex') ||
-							movie.overview.includes('romance') ||
-							movie.genre_ids.indexOf(10749) >= 0
-						) {
-							if ('title' in movie) {
-								if (
-									movie.title
-										.toLowerCase()
-										.includes(words[0]) ||
-									movie.title
-										.toLowerCase()
-										.includes(words[1]) ||
-									movie.title.toLowerCase().includes(words[2])
-								) {
-									return null;
+				<div className="d-flex">
+					<FilterBar handelFilter={this.handelFilter} />
+					<div className="row d-flex justify-content-around">
+						{this.state.moviesObj.map((movie) => {
+							if (
+								movie.overview.includes('sex') ||
+								movie.overview.includes('romance') ||
+								movie.genre_ids.indexOf(10749) >= 0
+							) {
+								if ('title' in movie) {
+									if (
+										movie.title
+											.toLowerCase()
+											.includes(words[0]) ||
+										movie.title
+											.toLowerCase()
+											.includes(words[1]) ||
+										movie.title
+											.toLowerCase()
+											.includes(words[2])
+									) {
+										return null;
+									}
+								} else if ('name' in movie) {
+									if (
+										movie.name
+											.toLowerCase()
+											.includes(words[0]) ||
+										movie.name
+											.toLowerCase()
+											.includes(words[1]) ||
+										movie.name
+											.toLowerCase()
+											.includes(words[2])
+									) {
+										return null;
+									}
 								}
-							} else if ('name' in movie) {
-								if (
-									movie.name
-										.toLowerCase()
-										.includes(words[0]) ||
-									movie.name
-										.toLowerCase()
-										.includes(words[1]) ||
-									movie.name.toLowerCase().includes(words[2])
-								) {
-									return null;
-								}
-							}
-						} else
-							return (
-								<MovieCard
-									key={movie.id}
-									{...movie}
-									linkPath={this.state.linkPath}
-								/>
-							);
-					})}
+							} else
+								return (
+									<MovieCard
+										key={movie.id}
+										{...movie}
+										linkPath={this.state.linkPath}
+									/>
+								);
+						})}
+					</div>
 				</div>
+
 				<Paginate
 					pageNum={this.state.pageNum}
 					handelNext={this.handelNext}
